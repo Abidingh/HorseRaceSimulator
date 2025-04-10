f7443639d1943940603e18520e0a688e6a055347
Rusel Segmenov <rusirus2006@gmail.com>
2025-04-10 15:36:09 +0100

    Edited description of horse class, defined its variables and implemented the constructor for it.

--- Part1/Horse.java
+++ Part1/Horse.java
@@ -1,23 +1,33 @@
-
 /**
- * Write a description of class Horse here.
+ * The horse class is a blueprint for a horse that participates in the race.
+ * Each object tracks the horse's name, the symbol that appears in the race, the distance travelled by the horse in the race,
+ * the horse's confidence level i.e. how fast it will travel, and a flag that indicates if a horse has fallen or not.
+ * The class includes methods for moving the horse forward, checking if it has fallen, and resetting its position.
+ * The horse's confidence and symbol can also be changed with their respective mutator methods.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * 
+ * @author Rusel R Segmenov
+ * @version 1
  */
 public class Horse
 {
     //Fields of class Horse
-    
+    private String name;
+    private char symbol;
+    private double confidence;
+    private int distanceTravelled = 0;
+
+    private boolean fallFlag = false;
+
     
       
     //Constructor of class Horse
     /**
-     * Constructor for objects of class Horse
-     */
     public Horse(char horseSymbol, String horseName, double horseConfidence)
     {
-       
+        this.name = horseName;
+        this.symbol = horseSymbol;
+        this.confidence = horseConfidence;
     }
     
     
